fb612f0dab758fa4834418bb84dd63affc0e2872
Muhammad Alwan Naufal <mhmanau@gmail.com>
2020-09-15 06:50:18 +0700

    splash screen

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import { Splash } from './pages';
 
 const App = () => {
   return (
-    <View>
-      <Text>Welcome App JS</Text>
-    </View>
+    <Splash />
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
