0c50bd08b36bac8787b13529e5030fbf4e9cf314
Muhammad Alwan Naufal <mhmanau@gmail.com>
2020-09-15 10:38:24 +0700

    get started page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Splash } from './pages';
-
+import { GetStarted } from './pages';
 const App = () => {
   return (
-    <Splash />
+    <GetStarted />
   );
 };
 
